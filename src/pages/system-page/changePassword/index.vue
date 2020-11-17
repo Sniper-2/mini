3ca34b2086ee3bc4b7e9263a179cbeadad92ccd9
lirunliang <15889984943@163.com>
2020-11-17 20:17:05 +0800

    mini增加修改密码

--- src/pages/system-page/changePassword/index.vue
+++ src/pages/system-page/changePassword/index.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    修改密码
+  <div class=" contact-mode">
+    <el-form ref="form" label-position="left" :model="contactData" label-width="100px">
+      <el-form-item label="旧密码：" required>
+        <el-input v-model="contactData.name" placeholder="请输入旧密码"></el-input>
+      </el-form-item>
+      <el-form-item label="新密码：" required>
+        <el-input v-model="contactData.name" placeholder="请输入新密码"></el-input>
+      </el-form-item>
+      <el-form-item label="确认新密码：" required>
+        <el-input v-model="contactData.name" placeholder="请再次输入新密码"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">修改密码</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
@@ -14,7 +27,10 @@ export default {
   },
   data() {
     return {
+      contactData: {
+        name: '',
 
+      }
     };
   },
   computed: {
@@ -35,6 +51,20 @@ export default {
 };
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.contact-mode {
+  padding: 30px;
+  width: 400px;
+  border-radius: 8px;
+  border: 1px solid #ccc;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translateY(-50%) translateX(-50%);
+  /deep/ {
+    .el-form-item__label {
+      padding: 0;
+    }
+  }
+}
 </style>
--- src/pages/system-page/contactMode/index.vue
+++ src/pages/system-page/contactMode/index.vue
@@ -1,6 +1,22 @@
 <template>
-  <div>
-    联系方式
+  <div class=" contact-mode">
+    <el-form ref="form" label-position="left" :model="contactData" label-width="100px">
+      <el-form-item label="联系人：" required>
+        <el-input v-model="contactData.name" placeholder="请输入联系人姓名"></el-input>
+      </el-form-item>
+      <el-form-item label="联系电话：" required>
+        <el-input v-model="contactData.name" placeholder="请输入联系电话"></el-input>
+      </el-form-item>
+      <el-form-item label="校验地址：" required>
+        <el-input v-model="contactData.name" placeholder="请输入校验地址"></el-input>
+      </el-form-item>
+      <el-form-item label="营业时间：" required>
+        <el-input v-model="contactData.name" placeholder="请输入营业时间"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">更新资料</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
@@ -14,7 +30,10 @@ export default {
   },
   data() {
     return {
+      contactData: {
+        name: '',
 
+      }
     };
   },
   computed: {
@@ -35,6 +54,20 @@ export default {
 };
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.contact-mode {
+  padding: 30px;
+  width: 400px;
+  border-radius: 8px;
+  border: 1px solid #ccc;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translateY(-50%) translateX(-50%);
+  /deep/ {
+    .el-form-item__label {
+      padding: 0;
+    }
+  }
+}
 </style>
